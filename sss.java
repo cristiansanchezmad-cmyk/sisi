82903360390a0c5df5d3d6113112bc1335778288
A707\dam1 <cristiansanchezmad@gmail.com>
2025-10-20 15:40:36 +0200

    sisiiss

---
+++ sss.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class sss {
+    static void main() {
+                String palabra="Patricia";
+                switch (palabra){
+                    case "Raul":
+                        System.out.println("El alumno no es Raul");
+                        break;
+                    case "Patricia":
+                        System.out.println("La profe es Patricia");
+                        break;
+                    default:
+                        System.out.println("No sé el nombre");
+                        break;
+                }
+            }
+        }
+
+    }
+}
